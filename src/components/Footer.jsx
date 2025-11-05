4774c9dccfa278c5ff578c8d3cd77a44774e8b44
Ezhil-Dev <johnnydepp1853@gmail.com>
2025-11-05 16:10:14 +0530

    height adjt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-      <div className='bg-black/30 h-[1px] max-w-7xl mx-auto '></div>
+      <div className='bg-black/30 h-[1px] max-w-6xl mx-auto '></div>
       <div className='  mx-auto font-[Poppins] text-sm text-gray-500  text-center md:flex items-center justify-between px-10 md:px-20 md:py-4 py-2'>
         <a href='https://vishnuads.com/' className='hover:text-amber-600 font-Poppins mb-1' > Developed by <span className='underline '>Vishnu Ads & Ventures</span> </a >
         <p className='text-gray-600 mb-1'>🤝 Partnered with Cinema Factory 💖</p>
